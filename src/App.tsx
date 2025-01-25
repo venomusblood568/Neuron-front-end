2618811506b33c5fa2715ee1a98df31b01acf4d7
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-01-25 19:23:15 +0530

    Finally tailwind v4 setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import './App.css'
 
 function App() {
@@ -6,7 +5,6 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
