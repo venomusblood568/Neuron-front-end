cda79a9b2e398c5944fec7a3a509d220e679cb59
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-01-27 15:01:12 +0530

    Button issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Button } from "./Components/button";
+import "./App.css";
 
 function App() {
  return (
    <>
+     <Button
+       variant="primary"
+       text="Primary Button"
+       startIcon={<span>🚀</span>}
+     />
+
+     {/* Secondary Button */}
+     <Button
+       variant="secondary"
+       text="Secondary Button"
+       endIcon={<span>👉</span>}
+     />
    </>
  );
 }
