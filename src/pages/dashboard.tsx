78d3fd900ca722cea54961ca24f9955ee380669d
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-01-30 11:57:36 +0530

    pages

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,49 @@
+import { Button } from "../Components/button";
+import { ShareIcon } from "../icon/shareIcon";
+import { PlusIcon } from "../icon/plusIcon";
+import { Card } from "../Components/card";
+import { CreateContentModel } from "../Components/createpop";
+import { useState } from "react";
+import { SideBar } from "../Components/sidebar";
+
+export function Dashboard() {
+  const [modelOpen, setModelOpen] = useState(false);
+
+
+  return (
+    <div>
+      <SideBar />
+      <div className="p-4 bg-black ml-72 min-screen ">
+        <CreateContentModel
+          open={modelOpen}
+          onClose={() => {
+            setModelOpen(false);
+          }}
+        />
+        <div className="flex justify-end gap-4">
+          <Button variant="secondary" text="Share" startIcon={<ShareIcon />} />
+          <Button
+            variant="primary"
+            text="Add Content"
+            startIcon={<PlusIcon />}
+            onClick={() => setModelOpen(true)}
+          />
+        </div>
+        <div className="flex gap-4 py-1">
+          <Card
+            type="twitter"
+            link="https://x.com/Duckyatduck/status/1884305325039444338"
+            title="test1"
+          />
+          <Card
+            type="youtube"
+            link="https://www.youtube.com/watch?v=lfVCH6JUxmw"
+            title="Best of Rajesh Khanna Hindi Songs"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
