06a63c74911de14c04f77f7650eb18269721f19c
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-02-24 01:23:14 +0530

    cardpop fix

--- src/Components/createpop.tsx
+++ src/Components/createpop.tsx
@@ -1,12 +1,28 @@
+import { useEffect, useRef} from "react";
 import { CrossIcon } from "../icon/cross";
 import { Button } from "./button";
 
 export function CreateContentModel({ open, onClose }) {
+  const modalRef = useRef(null);
+  useEffect(() =>{
+    function handleClickOutside(event) {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        onClose();
+      }
+    }
+    if(open){
+      document.addEventListener("mousedown",handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+  },[open,onClose])
+  
   return (
     <div>
       {open && (
         <div className="w-screen h-screen bg-black fixed top-0 left-0 opacity-90 flex justify-center">
-          <div className="flex flex-col justify-center">
+          <div ref={modalRef} className="flex flex-col justify-center">
             <span className="bg-white opacity-100 p-4 rounded">
               <div className="flex justify-end">
                 <div onClick={onClose} className="cursor-pointer">
@@ -28,7 +44,7 @@ export function CreateContentModel({ open, onClose }) {
   );
 }
 
-function Input({ onChange, placeholder}: { onChange: () => void }) {
+function Input({ onChange, placeholder}) {
   return (
     <div>
       <input
