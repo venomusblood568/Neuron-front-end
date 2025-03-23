37bb28d61b400bfde5b0e24e037d1e86b161e65a
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-03-23 23:22:32 +0530

    mongo connect done

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,43 @@
 import { Button } from "../Components/button";
 import { Input } from "../Components/input";
-
+import { useRef } from "react";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
+import { useNavigate } from "react-router-dom";
 
 
 export function Login() {
   
+  const usernameRef = useRef<HTMLInputElement>();
+  const passwordRef = useRef<HTMLInputElement>();
+  const navigate = useNavigate();
+
+  async function login() {
+    const username = usernameRef.current?.value;
+    const password = passwordRef.current?.value;
+    const response = await axios.post(BACKEND_URL + "/api/v1/signin", {
+      username,
+      password,
+    });
+    const jwt = response.data.token
+    localStorage.setItem("token",jwt)
+    navigate("/dashboard");
+    
+  }
+
   return (
     <div className="h-screen w-screen bg-black flex justify-center items-center">
       <div className="bg-white rounded-xl min-w-72 p-8">
         <h1 className="text-center p-2 text-darkPurple text-[30px]">ᒪOᘜIᑎ</h1>
-        <Input placeholder="Username" />
-        <Input placeholder="Password"/>
+        <Input reference={usernameRef} placeholder="Username" />
+        <Input reference={passwordRef}  placeholder="Password" />
         <div className="flex justify-center pt-4 gap-3">
-          <Button loading={false} variant="primary" text="Login" />
+          <Button
+            onClick={login}
+            loading={false}
+            variant="primary"
+            text="Login"
+          />
         </div>
       </div>
     </div>
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,18 +1,40 @@
 import { Button } from "../Components/button";
 import { Input } from "../Components/input";
-
-
+import { useRef } from "react";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
+import { useNavigate } from "react-router-dom";
 
 export function SignUp() {
-  
+  const usernameRef = useRef<HTMLInputElement>()
+  const passwordRef = useRef<HTMLInputElement>()
+  const navigate = useNavigate();
+
+  async function signup(){
+    const username = usernameRef.current?.value;
+    const password = passwordRef.current?.value;
+    await axios.post(BACKEND_URL + "/api/v1/signup",{
+      username,
+      password
+    })
+    navigate("/login")
+    alert("You have signup up!!!")
+  }
+
+
   return (
     <div className="h-screen w-screen bg-black flex justify-center items-center">
       <div className="bg-white rounded-xl min-w-72 p-8">
         <h1 className="text-center p-2 text-darkPurple text-[30px]">SIGᑎᑌᑭ</h1>
-        <Input placeholder="Username"  />
-        <Input placeholder="Password"  />
+        <Input reference={usernameRef} placeholder="Username" />
+        <Input reference={passwordRef} placeholder="Password" />
         <div className="flex justify-center pt-4 gap-3">
-          <Button loading={false} variant="primary" text="Signup" />
+          <Button
+            onClick={signup}
+            loading={false}
+            variant="primary"
+            text="Signup"
+          />
         </div>
       </div>
     </div>
