bfb694bd3ca6f91f51eae6e2f25f4329a1111473
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-03-25 17:44:13 +0530

    Home page also done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Dashboard } from "./pages/dashboard";
+import { Home } from "./pages/home";
 import {Login } from "./pages/login";
 import { SignUp } from "./pages/signup";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
@@ -7,6 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element= {<Home/>}></Route>
         <Route path="/signup" element= {<SignUp/>}></Route>
         <Route path="/login" element= {<Login/>}></Route>
         <Route path="/dashboard" element= {<Dashboard/>}></Route>
---
+++ src/pages/home.tsx
@@ -0,0 +1,61 @@
+import { useNavigate } from "react-router-dom";
+
+export function Home() {
+  const navigate = useNavigate()
+  async function login() {
+    navigate("/login");
+   }
+    async function signup() {
+    navigate("/signup");
+    }
+
+  return (
+    <div className="relative h-screen w-full flex items-center justify-center text-center">
+      <div className="absolute inset-0 bg-[url('/neurons-7433381_1280.jpg')] bg-cover bg-center opacity-40"></div>
+      <div className="relative max-w-sm p-4 bg-black shadow-lg rounded-2xl">
+        <h2 className="text-xl font-bold">
+          <div className="flex items-center justify-center text-lightPurple text-3xl p-5 m-5 tracking-widest">
+            <a className="text-darkPurple">ᑎ</a>ᗴᑌᖇᗝ
+            <a className="text-darkPurple">ᑎ</a>
+          </div>
+        </h2>
+        <p className="text-gray-500 mt-2">
+          <strong>Neuron: Your Digital Second Brain 🧠✨</strong>
+          Neuron is a{" "}
+          <span className="font-semibold">second brain concept</span> website
+          where you can
+          <span className="text-darkPurple">
+            {" "}
+            store everything that resonates with you{" "}
+          </span>
+          whether it's a blog post, a random link, an Instagram post, YouTube
+          Shorts, or even your clothing shopping list.
+          <br />
+          <span className="font-semibold">
+            No more lost ideas or forgotten inspirations
+          </span>
+          —everything stays
+          <span className="text-darkPurple">
+            {" "}
+            organized, accessible, and ready whenever you need it.
+          </span>
+        </p>
+
+        <div className="flex items-center justify-center gap-4 py-2">
+          <button
+            onClick={signup}
+            className="mt-4 px-4 py-2 bg-darkPurple text-white rounded-lg hover:bg-lightPurple"
+          >
+            SignUp
+          </button>
+          <button
+            onClick={login}
+            className="mt-4 px-4 py-2 bg-darkPurple text-white rounded-lg hover:bg-lightPurple"
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
